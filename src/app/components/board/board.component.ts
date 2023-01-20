17c3f9925e2a91580caa2021a9f21ca5ee08d91d
Valeriia Zolotykh <98551132+ValeryZolotykh@users.noreply.github.com>
2023-01-20 14:13:31 +0200

    commit

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tr-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BoardComponent {
-
+  constructor() {}
+  ngOnInit(): void {}
 }
